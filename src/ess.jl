e058a0d30bc9de314246ba8fb653248d36eaba37
David Widmann <dev+github@devmotion.de>
2021-06-12 20:28:57 +0200

    eachslice is not available on Julia 1.0

--- src/ess.jl
+++ src/ess.jl
@@ -233,7 +233,7 @@ function ess_rhat(
     rhat = Vector{T}(undef, nparams)
 
     # for each parameter
-    for (i, chains_slice) in enumerate(eachslice(chains; dims = 2))
+    for (i, chains_slice) in enumerate((view(chains, :, i, :) for i in axes(chains, 2)))
         # check that no values are missing
         if any(x -> x === missing, chains_slice)
             rhat[i] = missing
