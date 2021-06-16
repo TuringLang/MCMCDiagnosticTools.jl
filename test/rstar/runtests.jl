69553239a7011faeba282e59d8cd2d5975358463
David Widmann <devmotion@users.noreply.github.com>
2021-06-16 12:35:01 +0200

    Return actual R* distribution (#7)

--- test/rstar/runtests.jl
+++ test/rstar/runtests.jl
@@ -1,47 +1,79 @@
 using InferenceDiagnostics
+
+using Distributions
+using MLJBase
 using MLJModels
 
-using Statistics
 using Test
 
+XGBoost = @load XGBoostClassifier verbosity = 0
+@pipeline XGBoost name = XGBoostDeterministic operation = predict_mode
+SVC = @load SVC verbosity = 0
+
 @testset "rstar.jl" begin
-    XGBoost = @load XGBoostClassifier
-    classifier = XGBoost()
+    classifiers = (XGBoost(), XGBoostDeterministic(), SVC())
     N = 1_000
 
-    @testset "examples" begin
-        # Compute R* statistic for a mixed chain.
+    @testset "examples (classifier = $classifier)" for classifier in classifiers
+        # Compute R⋆ statistic for a mixed chain.
         samples = randn(N, 2)
-        R = rstar(classifier, randn(N, 2), rand(1:3, N))
+        dist = rstar(classifier, randn(N, 2), rand(1:3, N))
 
-        # Resulting R value should be close to one, i.e. the classifier does not perform better than random guessing.
-        @test Statistics.mean(R) ≈ 1 rtol = 0.15
+        # Mean of the statistic should be focused around 1, i.e., the classifier does not
+        # perform better than random guessing.
+        if classifier isa MLJModels.Deterministic
+            @test dist isa Float64
+        else
+            @test dist isa LocationScale
+            @test dist.ρ isa PoissonBinomial
+            @test minimum(dist) == 0
+            @test maximum(dist) == 3
+        end
+        @test mean(dist) ≈ 1 rtol = 0.2
 
-        # Compute R* statistic for a mixed chain.
+        # Compute R⋆ statistic for a mixed chain.
         samples = randn(4 * N, 8)
         chain_indices = repeat(1:4, N)
-        R = rstar(classifier, samples, chain_indices)
+        dist = rstar(classifier, samples, chain_indices)
 
-        # Resulting R value should be close to one, i.e. the classifier does not perform better than random guessing.
-        @test Statistics.mean(R) ≈ 1 rtol = 0.15
+        # Mean of the statistic should be closte to 1, i.e., the classifier does not perform
+        # better than random guessing.
+        if classifier isa MLJModels.Deterministic
+            @test dist isa Float64
+        else
+            @test dist isa LocationScale
+            @test dist.ρ isa PoissonBinomial
+            @test minimum(dist) == 0
+            @test maximum(dist) == 4
+        end
+        @test mean(dist) ≈ 1 rtol = 0.15
 
-        # Compute R* statistic for a non-mixed chain.
+        # Compute the R⋆ statistic for a non-mixed chain.
         samples = [
             sin.(1:N) cos.(1:N)
             100 .* cos.(1:N) 100 .* sin.(1:N)
         ]
         chain_indices = repeat(1:2; inner=N)
+        dist = rstar(classifier, samples, chain_indices)
 
-        # Restuling R value should be close to two, i.e. the classifier should be able to learn an almost perfect decision boundary between chains.
-        R = rstar(classifier, samples, chain_indices)
-        @test Statistics.mean(R) ≈ 2 rtol = 5e-2
+        # Mean of the statistic should be close to 2, i.e., the classifier should be able to
+        # learn an almost perfect decision boundary between chains.
+        if classifier isa MLJModels.Deterministic
+            @test dist isa Float64
+        else
+            @test dist isa LocationScale
+            @test dist.ρ isa PoissonBinomial
+            @test minimum(dist) == 0
+            @test maximum(dist) == 2
+        end
+        @test mean(dist) ≈ 2 rtol = 0.15
     end
 
-    @testset "exceptions" begin
+    @testset "exceptions (classifier = $classifier)" for classifier in classifiers
         @test_throws DimensionMismatch rstar(classifier, randn(N - 1, 2), rand(1:3, N))
-        for iterations in (-13, -1, 0)
+        for subset in (-0.3, 0, 1 / (3 * N), 1 - 1 / (3 * N), 1, 1.9)
             @test_throws ArgumentError rstar(
-                classifier, randn(N, 2), rand(1:3, N); iterations=iterations
+                classifier, randn(N, 2), rand(1:3, N); subset=subset
             )
         end
     end
