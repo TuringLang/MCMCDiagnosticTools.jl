2a2be1de30bdcd3746cedb5cea343a54f4420ca4
David Widmann <devmotion@users.noreply.github.com>
2021-06-22 00:21:28 +0200

    Extend documentation of FFTESSMethod (#14)

--- src/ess.jl
+++ src/ess.jl
@@ -28,6 +28,12 @@ the effective sample size of MCMC chains.
 
 The algorithm is the same as the one of [`ESSMethod`](@ref) but this method uses fast
 Fourier transforms (FFTs) for estimating the autocorrelation.
+
+!!! info
+    To be able to use this method, you have to load a package that implements the
+    [AbstractFFTs.jl](https://github.com/JuliaMath/AbstractFFTs.jl) interface such
+    as [FFTW.jl](https://github.com/JuliaMath/FFTW.jl) or
+    [FastTransforms.jl](https://github.com/JuliaApproximation/FastTransforms.jl).
 """
 struct FFTESSMethod <: AbstractESSMethod end
 
