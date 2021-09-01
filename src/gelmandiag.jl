90f502dec5f116f7c12a42df1aa285137dffe6af
Rik Huijzer <rikhuijzer@pm.me>
2021-09-01 18:28:57 +0200

    Copy information from Mamba.jl (#19)
    * Copy information from Mamba.jl
    
    * Fix latex and update license
    
    * Put weird formatting back
    
    * Fix rafterydiag
    
    * Fix line length
    
    * Small changes
    
    * Remove whitespace

--- src/gelmandiag.jl
+++ src/gelmandiag.jl
@@ -1,5 +1,3 @@
-#################### Gelman, Rubin, and Brooks Diagnostics ####################
-
 function _gelmandiag(psi::AbstractArray{<:Real,3}; alpha::Real=0.05)
     niters, nparams, nchains = size(psi)
     nchains > 1 || error("Gelman diagnostic requires at least 2 chains")
@@ -56,7 +54,14 @@ end
 """
     gelmandiag(chains::AbstractArray{<:Real,3}; alpha::Real=0.95)
 
-Compute the Gelman, Rubin and Brooks diagnostics.
+Compute the Gelman, Rubin and Brooks diagnostics [^Gelman1992] [^Brooks1998].  Values of the
+diagnostic’s potential scale reduction factor (PSRF) that are close to one suggest
+convergence.  As a rule-of-thumb, convergence is rejected if the 97.5 percentile of a PSRF
+is greater than 1.2.
+
+[^Gelman1992]: Gelman, A., & Rubin, D. B. (1992). Inference from iterative simulation using multiple sequences. Statistical science, 7(4), 457-472.
+
+[^Brooks1998]: Brooks, S. P., & Gelman, A. (1998). General methods for monitoring convergence of iterative simulations. Journal of computational and graphical statistics, 7(4), 434-455.
 """
 function gelmandiag(chains::AbstractArray{<:Real,3}; kwargs...)
     estimates, upperlimits = _gelmandiag(chains; kwargs...)
