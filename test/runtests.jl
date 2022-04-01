eb10e4c1bbeb953a63523e11c8634c708f2d109d
Seth Axen <seth.axen@gmail.com>
2022-04-01 10:18:03 +0200

    Remove checks for old Julia versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,5 @@
 using Pkg
 
-# Activate test environment on older Julia versions
-@static if VERSION < v"1.2"
-    Pkg.activate(@__DIR__)
-    Pkg.develop(PackageSpec(; path=dirname(@__DIR__)))
-    Pkg.instantiate()
-end
-
 using MCMCDiagnosticTools
 using FFTW
 
@@ -39,16 +32,15 @@ Random.seed!(1)
         include("rafterydiag.jl")
     end
     @testset "R⋆ diagnostic" begin
-        # MLJXGBoostInterface requires Julia >= 1.3
         # XGBoost errors on 32bit systems: https://github.com/dmlc/XGBoost.jl/issues/92
-        if VERSION >= v"1.3" && Sys.WORD_SIZE == 64
+        if Sys.WORD_SIZE == 64
             # run tests related to rstar statistic
             Pkg.activate("rstar")
             Pkg.develop(; path=dirname(dirname(pathof(MCMCDiagnosticTools))))
             Pkg.instantiate()
             include(joinpath("rstar", "runtests.jl"))
         else
-            @info "R⋆ not tested: requires Julia >= 1.3 and a 64bit architecture"
+            @info "R⋆ not tested: requires a 64bit architecture"
         end
     end
 end
