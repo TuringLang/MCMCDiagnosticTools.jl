29cba7f04d0b7a623813ad4aeee1d22c68d5c3f3
Seth Axen <seth@sethaxen.com>
2022-11-19 22:14:45 +0100

    Update rstar tests with permuted dims

--- test/rstar/runtests.jl
+++ test/rstar/runtests.jl
@@ -15,8 +15,8 @@ const xgboost_deterministic = Pipeline(XGBoostClassifier(); operation=predict_mo
 
     @testset "examples (classifier = $classifier)" for classifier in classifiers
         # Compute R⋆ statistic for a mixed chain.
-        samples = randn(N, 2)
-        dist = rstar(classifier, randn(N, 2), rand(1:3, N))
+        samples = randn(2, N)
+        dist = rstar(classifier, samples, rand(1:3, N))
 
         # Mean of the statistic should be focused around 1, i.e., the classifier does not
         # perform better than random guessing.
@@ -31,7 +31,7 @@ const xgboost_deterministic = Pipeline(XGBoostClassifier(); operation=predict_mo
         @test mean(dist) ≈ 1 rtol = 0.2
 
         # Compute R⋆ statistic for a mixed chain.
-        samples = randn(4 * N, 8)
+        samples = randn(8, 4 * N)
         chain_indices = repeat(1:4, N)
         dist = rstar(classifier, samples, chain_indices)
 
@@ -48,10 +48,10 @@ const xgboost_deterministic = Pipeline(XGBoostClassifier(); operation=predict_mo
         @test mean(dist) ≈ 1 rtol = 0.15
 
         # Compute the R⋆ statistic for a non-mixed chain.
-        samples = [
+        samples = permutedims([
             sin.(1:N) cos.(1:N)
             100 .* cos.(1:N) 100 .* sin.(1:N)
-        ]
+        ])
         chain_indices = repeat(1:2; inner=N)
         dist = rstar(classifier, samples, chain_indices)
 
@@ -69,10 +69,10 @@ const xgboost_deterministic = Pipeline(XGBoostClassifier(); operation=predict_mo
     end
 
     @testset "exceptions (classifier = $classifier)" for classifier in classifiers
-        @test_throws DimensionMismatch rstar(classifier, randn(N - 1, 2), rand(1:3, N))
+        @test_throws DimensionMismatch rstar(classifier, randn(2, N - 1), rand(1:3, N))
         for subset in (-0.3, 0, 1 / (3 * N), 1 - 1 / (3 * N), 1, 1.9)
             @test_throws ArgumentError rstar(
-                classifier, randn(N, 2), rand(1:3, N); subset=subset
+                classifier, randn(2, N), rand(1:3, N); subset=subset
             )
         end
     end
