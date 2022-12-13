f76ced4cc59da54f58a8f9c601d1f00b5493423e
Seth Axen <seth@sethaxen.com>
2022-12-13 11:33:21 +0100

    Add split_chain_indices

--- src/utils.jl
+++ src/utils.jl
@@ -16,3 +16,38 @@ function indices_of_unique(x)
     return d
 end
 
+"""
+    split_chain_indices(
+        chain_inds::AbstractVector{Int},
+        nsplit::Int=2,
+    ) -> AbstractVector{Int}
+
+Split each chain in `chain_inds` into `nsplit` chains.
+
+For each chain in `chain_inds`, all entries are assumed to correspond to draws that have
+been ordered by iteration number. The result is a vector of the same length as `chain_inds`
+where each entry is the new index of the chain that the corresponding draw belongs to.
+"""
+function split_chain_indices(c::AbstractVector{<:Int}, nsplit::Int=2)
+    cnew = similar(c)
+    if nsplit == 1
+        copyto!(cnew, c)
+        return cnew
+    end
+    chain_indices = indices_of_unique(c)
+    chain_ind = 0
+    for chain in sort(collect(keys(chain_indices)))
+        inds = chain_indices[chain]
+        ndraws_per_split, rem = divrem(length(inds), nsplit)
+        ilast = 0
+        for j in 1:nsplit
+            chain_ind += 1
+            ndraws_this_split = ndraws_per_split + (j ≤ rem)
+            i = ilast + 1
+            ilast = i + ndraws_this_split - 1
+            @views cnew[inds[i:ilast]] .= chain_ind
+        end
+    end
+    return cnew
+end
+
--- test/utils.jl
+++ test/utils.jl
@@ -10,3 +10,24 @@ using Test
         @test all(inds[d[k]] .== k)
     end
 end
+
+@testset "split_chain_indices" begin
+    c = [2, 2, 1, 3, 4, 3, 4, 1, 2, 1, 4, 3, 3, 2, 4, 3, 4, 1, 4, 1]
+    MCMCDiagnosticTools.split_chain_indices(c, 1) == c
+
+    cnew = MCMCDiagnosticTools.split_chain_indices(c, 2)
+    d = MCMCDiagnosticTools.indices_of_unique(c)
+    dnew = MCMCDiagnosticTools.indices_of_unique(cnew)
+    for (i, inew) in enumerate(1:2:7)
+        @test length(dnew[inew]) ≥ length(dnew[inew + 1])
+        @test d[i] == vcat(dnew[inew], dnew[inew + 1])
+    end
+
+    cnew = MCMCDiagnosticTools.split_chain_indices(c, 3)
+    d = MCMCDiagnosticTools.indices_of_unique(c)
+    dnew = MCMCDiagnosticTools.indices_of_unique(cnew)
+    for (i, inew) in enumerate(1:3:11)
+        @test length(dnew[inew]) ≥ length(dnew[inew + 1]) ≥ length(dnew[inew + 2])
+        @test d[i] == vcat(dnew[inew], dnew[inew + 1], dnew[inew + 2])
+    end
+end
