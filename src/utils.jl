a59cd0db0dd6759ab20c14d31e7a4d2d8cdeacc2
Seth Axen <seth@sethaxen.com>
2022-12-14 00:29:44 +0100

    Add rank_normalize

--- src/utils.jl
+++ src/utils.jl
@@ -13,3 +13,32 @@ function split_chains(data::AbstractArray{<:Any,3}, split::Int=2)
     data_sub = @views data[begin:(end-niter_drop), :, :]
     return reshape(data_sub, ndraws_split, nchains_split, nparams)
 end
+
+"""
+    rank_normalize(x::AbstractVector)
+    rank_normalize(x::AbstractArray{<:Any,3}; dims=(1, 2))
+
+Rank-normalize the inputs `x` along the dimensions `dims`.
+
+Rank-normalization proceeds by first ranking the inputs using "tied ranking"
+and then transforming the ranks to normal quantiles so that the result is standard
+normally distributed.
+"""
+function rank_normalize(x::AbstractArray{<:Any,3}; dims=(1, 2))
+    # TODO: can we avoid mapslices and the allocations here?
+    return mapslices(x; dims=dims) do xi
+        return reshape(rank_normalize(vec(xi)), size(xi))
+    end
+end
+function rank_normalize(x::AbstractVector)
+    values = similar(x, float(eltype(x)))
+    rank_normalize!(values, x)
+    return values
+end
+function rank_normalize!(values, x)
+    rank = StatsBase.tiedrank(x)
+    _normal_quantiles_from_ranks!(values, rank)
+    values .= StatsFuns.norminvcdf.(values)
+    return values
+end
+
--- test/utils.jl
+++ test/utils.jl
@@ -1,9 +1,26 @@
 using MCMCDiagnosticTools
 using Test
+using Random
+using Statistics
 
 @testset "split_chains" begin
     x = rand(100, 4, 8)
     @test @inferred(MCMCDiagnosticTools.split_chains(x, 1)) == x
     @test MCMCDiagnosticTools.split_chains(x, 2) == reshape(x, 50, 8, 8)
     @test MCMCDiagnosticTools.split_chains(x, 3) == reshape(x[1:99, :, :], 33, 12, 8)
+end
+
+@testset "rank_normalize" begin
+    x = randn(1000) ./ randn.()  # cauchy draws
+    z = @inferred MCMCDiagnosticTools.rank_normalize(x)
+    @test size(z) == size(x)
+    @test mean(z) ≈ 0 atol=1e-13
+    @test std(z) ≈ 1 rtol=1e-2
+
+    x = randexp(1000, 4, 8)
+    @test_broken @inferred MCMCDiagnosticTools.rank_normalize(x)
+    z = MCMCDiagnosticTools.rank_normalize(x)
+    @test size(z) == size(x)
+    @test all(xi -> isapprox(xi, 0; atol=1e-13), mean(z; dims=(1, 2)))
+    @test all(xi -> isapprox(xi, 1; rtol=1e-2), std(z; dims=(1, 2)))
 end
