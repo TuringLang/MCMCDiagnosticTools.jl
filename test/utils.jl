69d561e54988d99184e10c264d448addaa638712
Seth Axen <seth@sethaxen.com>
2022-12-14 00:56:02 +0100

    Run formatter

--- test/utils.jl
+++ test/utils.jl
@@ -14,8 +14,8 @@ end
     x = randn(1000) ./ randn.()  # cauchy draws
     z = @inferred MCMCDiagnosticTools.rank_normalize(x)
     @test size(z) == size(x)
-    @test mean(z) ≈ 0 atol=1e-13
-    @test std(z) ≈ 1 rtol=1e-2
+    @test mean(z) ≈ 0 atol = 1e-13
+    @test std(z) ≈ 1 rtol = 1e-2
 
     x = randexp(1000, 4, 8)
     @test_broken @inferred MCMCDiagnosticTools.rank_normalize(x)
