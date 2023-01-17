899711e69abac4c93efef53a4b2ffa7ee2c12a07
Seth Axen <seth@sethaxen.com>
2023-01-17 01:05:00 +0100

    Increase tolerance of mcse tests

--- test/mcse.jl
+++ test/mcse.jl
@@ -21,7 +21,7 @@ using StatsBase
         # account for all but the 2 skipped checks
         nchecks =
             nparams * length(φs) * length(estimators) * length(dists) * length(mcse_methods)
-        α = (0.1 / nchecks) / 2  # multiple correction
+        α = (0.05 / nchecks) / 2  # multiple correction
         @testset for mcse in mcse_methods, f in estimators, dist in dists, φ in φs
             σ = sqrt(1 - φ^2) # ensures stationary distribution is N(0, 1)
             x = ar1(φ, σ, ndraws, nchains, nparams)
