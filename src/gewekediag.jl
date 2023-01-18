4d557162b74db98914cfd8bc2bf863c28f58257d
Seth Axen <seth@sethaxen.com>
2023-01-18 12:44:20 +0100

    Document kwargs passed to mcse

--- src/gewekediag.jl
+++ src/gewekediag.jl
@@ -12,6 +12,8 @@ samples are independent.  A non-significant test p-value indicates convergence. 
 p-values indicate non-convergence and the possible need to discard initial samples as a
 burn-in sequence or to simulate additional samples.
 
+`kwargs` are forwarded to [`mcse`](@ref).
+
 [^Geweke1991]: Geweke, J. F. (1991). Evaluating the accuracy of sampling-based approaches to the calculation of posterior moments (No. 148). Federal Reserve Bank of Minneapolis.
 """
 function gewekediag(x::AbstractVector{<:Real}; first::Real=0.1, last::Real=0.5, kwargs...)
--- src/heideldiag.jl
+++ src/heideldiag.jl
@@ -9,6 +9,8 @@ means are within a target ratio. Stationarity is rejected (0) for significant te
 Halfwidth tests are rejected (0) if observed ratios are greater than the target, as is the
 case for `s2` and `beta[1]`.
 
+`kwargs` are forwarded to [`mcse`](@ref).
+
 [^Heidelberger1983]: Heidelberger, P., & Welch, P. D. (1983). Simulation run length control in the presence of an initial transient. Operations Research, 31(6), 1109-1144.
 """
 function heideldiag(
