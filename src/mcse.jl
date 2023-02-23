4029eb8e68739a4876592834a8d4dd4a1b3815c4
Seth Axen <seth@sethaxen.com>
2023-02-23 21:39:23 +0100

    Define estimator keyword for mcse

--- src/mcse.jl
+++ src/mcse.jl
@@ -2,7 +2,7 @@ const normcdf1 = 0.8413447460685429  # StatsFuns.normcdf(1)
 const normcdfn1 = 0.15865525393145705  # StatsFuns.normcdf(-1)
 
 """
-    mcse(estimator, samples::AbstractArray{<:Union{Missing,Real}}; kwargs...)
+    mcse(samples::AbstractArray{<:Union{Missing,Real}}; estimator=Statistics.mean, kwargs...)
 
 Estimate the Monte Carlo standard errors (MCSE) of the `estimator` applied to `samples` of
 shape `(draws, chains, parameters)`.
@@ -36,18 +36,22 @@ by checking the bulk- and tail-[`ess_rhat`](@ref) values.
                doi: [10.1007/978-3-642-27440-4_18](https://doi.org/10.1007/978-3-642-27440-4_18)
 
 """
-mcse(f, x::AbstractArray{<:Union{Missing,Real},3}; kwargs...) = _mcse_sbm(f, x; kwargs...)
-function mcse(
+function mcse(x::AbstractArray{<:Union{Missing,Real},3}; estimator=Statistics.mean, kwargs...)
+    return _mcse(estimator, x; kwargs...)
+end
+
+_mcse(f, x; kwargs...) = _mcse_sbm(f, x; kwargs...)
+function _mcse(
     ::typeof(Statistics.mean), samples::AbstractArray{<:Union{Missing,Real},3}; kwargs...
 )
-    S = first(ess_rhat(Statistics.mean, samples; kwargs...))
+    S = ess(samples; estimator=Statistics.mean, kwargs...)
     return dropdims(Statistics.std(samples; dims=(1, 2)); dims=(1, 2)) ./ sqrt.(S)
 end
-function mcse(
+function _mcse(
     ::typeof(Statistics.std), samples::AbstractArray{<:Union{Missing,Real},3}; kwargs...
 )
     x = (samples .- Statistics.mean(samples; dims=(1, 2))) .^ 2  # expectand proxy
-    S = first(ess_rhat(Statistics.mean, x; kwargs...))
+    S = ess(x; type=Statistics.mean, kwargs...)
     # asymptotic variance of sample variance estimate is Var[var] = E[μ₄] - E[var]²,
     # where μ₄ is the 4th central moment
     # by the delta method, Var[std] = Var[var] / 4E[var] = (E[μ₄]/E[var] - E[var])/4,
@@ -56,13 +60,13 @@ function mcse(
     mean_moment4 = dropdims(Statistics.mean(abs2, x; dims=(1, 2)); dims=(1, 2))
     return @. sqrt((mean_moment4 / mean_var - mean_var) / S) / 2
 end
-function mcse(
+function _mcse(
     f::Base.Fix2{typeof(Statistics.quantile),<:Real},
     samples::AbstractArray{<:Union{Missing,Real},3};
     kwargs...,
 )
     p = f.x
-    S = first(ess_rhat(f, samples; kwargs...))
+    S = ess(samples; estimator=f, kwargs...)
     T = eltype(S)
     R = promote_type(eltype(samples), typeof(oneunit(eltype(samples)) / sqrt(oneunit(T))))
     values = similar(S, R)
@@ -71,10 +75,10 @@ function mcse(
     end
     return values
 end
-function mcse(
+function _mcse(
     ::typeof(Statistics.median), samples::AbstractArray{<:Union{Missing,Real},3}; kwargs...
 )
-    S = first(ess_rhat(Statistics.median, samples; kwargs...))
+    S = ess(samples; estimator=Statistics.median, kwargs...)
     T = eltype(S)
     R = promote_type(eltype(samples), typeof(oneunit(eltype(samples)) / sqrt(oneunit(T))))
     values = similar(S, R)
