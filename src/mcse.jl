f4d9f675ca991d58efd83f25b2a0bd89d10e9142
Seth Axen <seth@sethaxen.com>
2023-02-24 10:01:25 +0100

    Update mcse docstring

--- src/mcse.jl
+++ src/mcse.jl
@@ -7,15 +7,15 @@ const normcdfn1 = 0.15865525393145705  # StatsFuns.normcdf(-1)
 Estimate the Monte Carlo standard errors (MCSE) of the `estimator` applied to `samples` of
 shape `(draws, chains, parameters)`.
 
-See also: [`ess_rhat`](@ref)
+See also: [`ess`](@ref)
 
 ## Estimators
 
 `estimator` must accept a vector of the same `eltype` as `samples` and return a real estimate.
 
-For the following estimators, the effective sample size [`ess_rhat`](@ref) and an estimate
+For the following estimators, the effective sample size [`ess`](@ref) and an estimate
 of the asymptotic variance are used to compute the MCSE, and `kwargs` are forwarded to
-`ess_rhat`:
+`ess`:
 - `Statistics.mean`
 - `Statistics.median`
 - `Statistics.std`
@@ -26,7 +26,7 @@ is used as a fallback, and the only accepted `kwargs` are `batch_size`, which in
 size of the overlapping batches used to estimate the MCSE, defaulting to
 `floor(Int, sqrt(draws * chains))`. Note that SBM tends to underestimate the MCSE,
 especially for highly autocorrelated chains. One should verify that autocorrelation is low
-by checking the bulk- and tail-[`ess_rhat`](@ref) values.
+by checking the bulk- and tail-ESS values.
 
 [^FlegalJones2011]: Flegal JM, Jones GL. (2011) Implementing MCMC: estimating with confidence.
                     Handbook of Markov Chain Monte Carlo. pp. 175-97.
