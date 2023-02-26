d2980b6ac3589682b8f1dc36a7f592899f5aa540
Seth Axen <seth@sethaxen.com>
2023-02-26 15:46:27 +0100

    Rename `estimator` to `kind` for `mcse`

--- src/mcse.jl
+++ src/mcse.jl
@@ -2,16 +2,17 @@ const normcdf1 = 0.8413447460685429  # StatsFuns.normcdf(1)
 const normcdfn1 = 0.15865525393145705  # StatsFuns.normcdf(-1)
 
 """
-    mcse(samples::AbstractArray{<:Union{Missing,Real}}; estimator=Statistics.mean, kwargs...)
+    mcse(samples::AbstractArray{<:Union{Missing,Real}}; kind=Statistics.mean, kwargs...)
 
-Estimate the Monte Carlo standard errors (MCSE) of the `estimator` applied to `samples` of
-shape `(draws, chains, parameters)`.
+Estimate the Monte Carlo standard errors (MCSE) of the estimator `kind` applied to `samples`
+of shape `(draws, chains, parameters)`.
 
 See also: [`ess`](@ref)
 
-## Estimators
+## Kinds of MCSE estimates
 
-`estimator` must accept a vector of the same `eltype` as `samples` and return a real estimate.
+The estimator whose MCSE should be estimator is specified with `kind`. `kind` must accept a
+vector of the same `eltype` as `samples` and return a real estimate.
 
 For the following estimators, the effective sample size [`ess`](@ref) and an estimate
 of the asymptotic variance are used to compute the MCSE, and `kwargs` are forwarded to
@@ -36,24 +37,22 @@ by checking the bulk- and tail-ESS values.
                doi: [10.1007/978-3-642-27440-4_18](https://doi.org/10.1007/978-3-642-27440-4_18)
 
 """
-function mcse(
-    x::AbstractArray{<:Union{Missing,Real},3}; estimator=Statistics.mean, kwargs...
-)
-    return _mcse(estimator, x; kwargs...)
+function mcse(x::AbstractArray{<:Union{Missing,Real},3}; kind=Statistics.mean, kwargs...)
+    return _mcse(kind, x; kwargs...)
 end
 
 _mcse(f, x; kwargs...) = _mcse_sbm(f, x; kwargs...)
 function _mcse(
     ::typeof(Statistics.mean), samples::AbstractArray{<:Union{Missing,Real},3}; kwargs...
 )
-    S = ess(samples; estimator=Statistics.mean, kwargs...)
+    S = _ess(Statistics.mean, samples; kwargs...)
     return dropdims(Statistics.std(samples; dims=(1, 2)); dims=(1, 2)) ./ sqrt.(S)
 end
 function _mcse(
     ::typeof(Statistics.std), samples::AbstractArray{<:Union{Missing,Real},3}; kwargs...
 )
     x = (samples .- Statistics.mean(samples; dims=(1, 2))) .^ 2  # expectand proxy
-    S = ess(x; estimator=Statistics.mean, kwargs...)
+    S = _ess(Statistics.mean, x; kwargs...)
     # asymptotic variance of sample variance estimate is Var[var] = E[μ₄] - E[var]²,
     # where μ₄ is the 4th central moment
     # by the delta method, Var[std] = Var[var] / 4E[var] = (E[μ₄]/E[var] - E[var])/4,
@@ -68,7 +67,7 @@ function _mcse(
     kwargs...,
 )
     p = f.x
-    S = ess(samples; estimator=f, kwargs...)
+    S = _ess(f, samples; kwargs...)
     T = eltype(S)
     R = promote_type(eltype(samples), typeof(oneunit(eltype(samples)) / sqrt(oneunit(T))))
     values = similar(S, R)
@@ -80,7 +79,7 @@ end
 function _mcse(
     ::typeof(Statistics.median), samples::AbstractArray{<:Union{Missing,Real},3}; kwargs...
 )
-    S = ess(samples; estimator=Statistics.median, kwargs...)
+    S = _ess(Statistics.median, samples; kwargs...)
     T = eltype(S)
     R = promote_type(eltype(samples), typeof(oneunit(eltype(samples)) / sqrt(oneunit(T))))
     values = similar(S, R)
