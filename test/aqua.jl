122f79f704f2fbf43960b566ffce49433d6d9819
David Widmann <devmotion@users.noreply.github.com>
2023-11-16 01:21:33 +0100

    Update Aqua tests

--- test/aqua.jl
+++ test/aqua.jl
@@ -5,12 +5,6 @@ using Test
 @testset "Aqua" begin
     # Test ambiguities separately without Base and Core
     # Ref: https://github.com/JuliaTesting/Aqua.jl/issues/77
-    # Only test Project.toml formatting on Julia > 1.6 when running Github action
-    # Ref: https://github.com/JuliaTesting/Aqua.jl/issues/105
-    Aqua.test_all(
-        MCMCDiagnosticTools;
-        ambiguities=false,
-        project_toml_formatting=VERSION >= v"1.7" || !haskey(ENV, "GITHUB_ACTIONS"),
-    )
-    Aqua.test_ambiguities([MCMCDiagnosticTools])
+    Aqua.test_all(MCMCDiagnosticTools; ambiguities=false)
+    Aqua.test_ambiguities(MCMCDiagnosticTools)
 end
