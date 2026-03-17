071f3e3c00cd4716496065f863a796dd48ac1955
Seth Axen <seth@sethaxen.com>
2026-03-17 10:03:53 +0100

    docs: Move r-hat kind docs to variable and add extended help

--- src/ess_rhat.jl
+++ src/ess_rhat.jl
@@ -6,6 +6,19 @@ const _DOC_SPLIT_CHAINS = """`split_chains` indicates the number of chains each 
                           `d = mod(draws, split_chains) > 0`, i.e. the chains cannot be evenly split, then 1 draw
                           is discarded after each of the first `d` splits within each chain."""
 
+const _DOC_RHAT_KIND = """
+## Kinds of ``\\widehat{R}``
+
+The following `kind`s are supported:
+- `:rank`: maximum of ``\\widehat{R}`` with `kind=:bulk` and `kind=:tail`.
+- `:bulk`: basic ``\\widehat{R}`` computed on rank-normalized draws. This kind diagnoses
+    poor convergence in the bulk of the distribution due to trends or different locations of
+    the chains.
+- `:tail`: ``\\widehat{R}`` computed on draws folded around the median and then
+    rank-normalized. This kind diagnoses poor convergence in the tails of the distribution
+    due to different scales of the chains.
+- `:basic`: Classic ``\\widehat{R}``."""
+
 """
     AutocovMethod <: AbstractAutocovMethod
 
@@ -303,29 +316,21 @@ end
 Compute the ``\\widehat{R}`` diagnostics for each parameter in `samples` of shape
 `(draws, [chains[, parameters...]])`.[^VehtariGelman2021]
 
-`kind` indicates the kind of ``\\widehat{R}`` to compute (see below).
-
-$_DOC_SPLIT_CHAINS
+`kind` indicates the kind of ``\\widehat{R}`` to compute (see extended help).
 
 See also [`ess`](@ref), [`ess_rhat`](@ref), [`rhat_nested`](@ref), [`rstar`](@ref)
-
-## Kinds of ``\\widehat{R}``
-
-The following `kind`s are supported:
-- `:rank`: maximum of ``\\widehat{R}`` with `kind=:bulk` and `kind=:tail`.
-- `:bulk`: basic ``\\widehat{R}`` computed on rank-normalized draws. This kind diagnoses
-    poor convergence in the bulk of the distribution due to trends or different locations of
-    the chains.
-- `:tail`: ``\\widehat{R}`` computed on draws folded around the median and then
-    rank-normalized. This kind diagnoses poor convergence in the tails of the distribution
-    due to different scales of the chains.
-- `:basic`: Classic ``\\widehat{R}``.
 
 [^VehtariGelman2021]: Vehtari, A., Gelman, A., Simpson, D., Carpenter, B., & Bürkner, P. C. (2021).
     Rank-normalization, folding, and localization: An improved ``\\widehat {R}`` for
     assessing convergence of MCMC. Bayesian Analysis.
     doi: [10.1214/20-BA1221](https://doi.org/10.1214/20-BA1221)
     arXiv: [1903.08008](https://arxiv.org/abs/1903.08008)
+
+# Extended Help
+
+$_DOC_SPLIT_CHAINS
+
+$_DOC_RHAT_KIND
 """
 function rhat(samples::AbstractArray{<:Union{Missing,Real}}; kind::Symbol=:rank, kwargs...)
     # if we just call _rhat(Val(kind), ...) Julia cannot infer the return type with default
